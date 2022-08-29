32f9bffb1c4c63f8c40bf5ae8f05d2cc88239a00
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 14:39:54 -0500

    user

--- echo.sh
+++ echo.sh
@@ -2,7 +2,7 @@ echo hello
 echo -e "hello\nworld"
 echo -e "one\ttwo"
 a=10
-echo value of a is $a
+echo value of a is a
 date1=$(date +%F)
 
 echo todays date is $date1
--- if.sh
+++ if.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+!/usr/bin/bash
 if [ abc == ABC ]
 then
  echo "both are same"
